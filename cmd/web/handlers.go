3e3151e86a170516a2ecce3f7227228afdc51a74
meeklife <patjhealth36@gmail.com>
2024-10-04 14:35:22 +0100

    serving static files with fileserver

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -39,15 +39,12 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	// w.Write([]byte("Display a snippet..."))
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		// w.WriteHeader(405)
-		// w.Write([]byte("Method not allowed"))
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
