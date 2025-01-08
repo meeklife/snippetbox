f7bccedae8cd2ee7c98c6b652ce177f3953f808e
meeklife <forsonbaah@gmail.com>
2025-01-08 13:39:07 +0000

    add custom template function to format time&date

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,13 +3,23 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"meeklife.net/snippetbox/pkg/models"
 )
 
 type templateData struct {
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
+	CurrentYear int
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -24,7 +34,7 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
