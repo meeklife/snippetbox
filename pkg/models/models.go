2bdd2dcac09f15ed29a68d8d238d9a8feef15f04
meeklife <forsonbaah@gmail.com>
2025-01-17 13:32:21 +0000

    building the user model

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -5,7 +5,11 @@ import (
 	"time"
 )
 
-var ErrNoRecord = errors.New("models: No matching records found")
+var (
+	ErrNoRecord           = errors.New("models: No matching records found")
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	ErrDuplicateEmail     = errors.New("models: duplicate email")
+)
 
 type Snippet struct {
 	ID      int
@@ -13,4 +17,13 @@ type Snippet struct {
 	Content string
 	Created time.Time
 	Expires time.Time
+}
+
+type User struct {
+	ID             int
+	Name           string
+	Email          string
+	HashedPassword []byte
+	Create         time.Time
+	Active         bool
 }
